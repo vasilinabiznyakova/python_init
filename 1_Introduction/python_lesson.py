caa3b9841fb07f1850542e01c031e2723973f7e1
Vasilina Biznyakova <vbiznyakova@gmail.com>
2025-06-03 21:11:25 +0200

    add info for lesson 1

---
+++ 1_Introduction/python_lesson.py
@@ -0,0 +1,83 @@
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+
+
+# chat bots
+# any website
+# devops
+# admin panels
+# servers
+# AI => a lot of libraries for teaching models
+# satellites code
+
+# Any computer was started from calculator operating 0 and 1
+
+# Python is being translated to machine code with interpreter, compilation 
+  
+ # pycharm - is more complex and russian founders JetBrains
+
+ # win r cmd  python exit()
+
+ # code . inside folder will open project in VS code 
+
+# ctrl * alt * up/down
+
+# normal devision / 
+
+# devision //
+
+# % 
+
+# оперативна память  RAM memory Hard drive/memory persisten
+
+# variable interp
+
+# SyntaxError: expected ':'
+# >>> id(age)
+# 140723285400536 // address in memory
+
+# Interpreter automatically removes variables that are no longer used — done by the garbage collector.
+
+# >>> type(age)
+# <class 'int'>
+
+
+
+# multiline doc string
+message = """
+
+Hello
+
+
+Welcome to game
+
+Vas!
+"""
+
+#snake_case # convention 
+is_user_authenticated = True
+#camelCase # will work but not by protocol
+#PascalCase will work but not by protocol
+#kebab-case will work but not by protocol
+print(message)
+
+#приведение типов
+str(1)
+str(1,6) # TypeError: str() argument 'encoding' must be str, not int
+
+int("12")
+int("12.2") # ValueError: invalid literal for int() with base 10: '12.2'
+
+# concatenation >>> "2" + "2"
+# '22'
+
+# >>> "A" * 10
+# 'AAAAAAAAAA'
+
+print("*" * 100)
+
+# >>> bool(None)
+# False
+
+round (2.333333333, 2)
