6f68192b02cf716261f22e1512d3e351117fc6a8
Vasilina Biznyakova <vbiznyakova@gmail.com>
2025-06-07 14:44:22 +0200

    add get_number_from_user function

--- 2_control_structures_exceptions/practice_1.py
+++ 2_control_structures_exceptions/practice_1.py
@@ -12,11 +12,21 @@ Enter daily spent
 
 # weekly_budget = input("Enter your weekly budget: ")
 # weekly_budget = int(weekly_budget)
+def get_number_from_user():
+    try:
+        number = int(input("Enter number: "))
+    except ValueError:
+        print("Number value is not correct. We are setting budget to 100...")
+    number = 100
 
-try:
-    weekly_budget = int(input("Enter your weekly budget: "))
-except ValueError:
-    print("Weekly budget value is not correct. We are setting budget to 100...")
-    weekly_budget = 100
-    
-print(weekly_budget + 100)
+    return number
+
+# try:
+#     weekly_budget = int(input("Enter your weekly budget: "))
+# except ValueError:
+#     print("Weekly budget value is not correct. We are setting budget to 100...")
+#     weekly_budget = 100
+
+print("Enter weekly budget")
+result = get_number_from_user()
+print(result + 100)
